09cd386a57a05f2d2efe6b45731b170c58b4d4bb
dreamfall <amarant.st@gmail.com>
2011-10-17 00:21:39 +0300

    session initializer fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,7 +59,7 @@ namespace :deploy do
   end
   desc "Start application"
   task :start, :roles => :app do
-    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+    run "bundle exec #{unicorn_rails} -Dc #{unicorn_conf}"
   end
 
   desc "Stop application"
@@ -69,6 +69,6 @@ namespace :deploy do
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || bundle exec #{unicorn_rails} -Dc #{unicorn_conf}"
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Bsuir::Application.config.session_store :cookie_store, key: '_bsuir_session'
+Bsuir::Application.config.session_store :cookie_store, key => 'd_bsuir_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
