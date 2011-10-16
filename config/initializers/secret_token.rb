c92ff75ef198f752905cc1b961e764b2eab457ae
dreamfall <amarant.st@gmail.com>
2011-10-16 20:31:40 +0300

    project + added capfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bsuir::Application.config.secret_token = 'b747f0d5f7e267b71422d502ed1c4e692a124f24137b9e30d4c22c6b6abb4bbe25284b9c2f62f2e6402f4d783889d99fb7ead9a36d1aa976019d382ac1da2a2d'
